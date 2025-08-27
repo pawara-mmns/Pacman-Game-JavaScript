d874e1e5fcce1ab886af60678e9f96d4308e34fb
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-08-27 15:02:53 +0530

    add image variables for ghosts and Pac-Man

--- pacman.js
+++ pacman.js
@@ -7,9 +7,27 @@ const boardwidth = columnCount * tileSize;
 const boardheight = rowCount * tileSize;
 let context;
 
+//images
+let blueGhostImage;
+let orangeGhostImage;
+let pinkGhostImage;
+let redGhostImage;
+let pacmanUpImage
+let pacmanDownImage;
+let pacmanLeftImage;
+let pacmanRightImage;
+let wallImage;
+
+// let pacmanImage;
+// let ghostImage;
+// let fruitImage;
+
+
+
 window.onload = function(){
     board = document.getElementById("board");
     board.height = boardheight;
     board.width = boardwidth;
     context = board.getContext("2d");
 }
+
