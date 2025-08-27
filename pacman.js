68f520ff206e11d1346bbbc1e17f4229e943c9dc
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-08-27 15:09:06 +0530

    add loadImages function to initialize game assets

--- pacman.js
+++ pacman.js
@@ -31,3 +31,36 @@ window.onload = function(){
     context = board.getContext("2d");
 }
 
+function loadImages(){
+    wallImage = new Image();
+    wallImage.src = "images/wall.png";
+
+    blueGhostImage = new Image();
+    blueGhostImage.src = "images/blueGhost.png";
+
+    orangeGhostImage = new Image();
+    orangeGhostImage.src = "images/orangeGhost.png";
+
+    pinkGhostImage = new Image();
+    pinkGhostImage.src = "images/pinkGhost.png";
+
+    redGhostImage = new Image();
+    redGhostImage.src = "images/redGhost.png";
+
+    //pac-man images
+
+    pacmanUpImage = new Image();
+    pacmanUpImage.src = "images/pacmanUp.png";
+
+    pacmanDownImage = new Image();
+    pacmanDownImage.src = "images/pacmanDown.png";
+
+    pacmanLeftImage = new Image();
+    pacmanLeftImage.src = "images/pacmanLeft.png";
+
+    pacmanRightImage = new Image();
+    pacmanRightImage.src = "images/pacmanRight.png";
+
+    
+}
+
