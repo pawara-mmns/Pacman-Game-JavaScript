1a6278216774167d96a63235a1c9b54ea71278d6
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-08-28 10:21:51 +0530

    add Block class to manage game elements with properties for image and position

--- pacman.js
+++ pacman.js
@@ -112,3 +112,16 @@ function loadImages(){
     
 }
 
+class Block {
+    constructor(image, x, y, width, height){
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.startX = x;
+        this.startY = y;
+    }
+}
+
